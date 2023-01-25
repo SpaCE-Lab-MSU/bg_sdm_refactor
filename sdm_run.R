df67dff0f5d4c909d803af987db7b5c5e527efc8
Pat Bills <billspat@users.noreply.github.com>
2023-01-24 23:41:38 -0500

    CLI runner script

--- sdm_run.R
+++ sdm_run.R
@@ -1,52 +1,61 @@
 #!/usr/bin/env Rscript
 
 ## sdm_run.R
+# this does one run of a species, for a specific radius an d uses run number to name outputs
 
+# requires the following to bet set in the environment
+
+# SDM_BASE_PATH
+# SDM_OCCS_PATH sub dir od base path
+# SDM_ENVS_PATH= sub dir of base path
+# sprintf patterns to name these files
+# SDM_OCCS_PATH_TEMPLATE='occurrence_records/%s_thinned_full'
+# SDM_OCCS_FILE_TEMPLATE='%s_thinned_wallace.csv'
+
+# TODO convert these all to command line params
 # main program
 # runs only when script is run by itself e.g from Rscript
 # uses the 
 if (sys.nframe() == 0){
-  
-  thisScript <- basename(sys.frame(1)$ofile)
-  usage <- paste(thisScript,"<Genus_species> <radiuskm[default 1]> <number of runs[default 1>]")
+  source('geodiv_model_eval.R')
+  usage <- "Rscript --vanilla sdm_run.R <Genus_species> <radiuskm[default 1]> <number of runs[default 1]> output_path full path to output)"
   help <- "set OS env variables SDM_BASE_PATH to main folder with data and SDM_ENVS_PATH to sub folder where rasters are"               
   
-  args = commandArgs(trailingOnly= FALSE)
+  args = commandArgs(trailingOnly= TRUE)
+  
+  print(args)
+  
   # test if there are enough arguments: if not, return an error
-  if (length(args) < 3) {
+  if (length(args) < 4) {
     stop(usage, call.=FALSE)
   } else { 
-    species  = args[1]
-    radius   = args[2]
-    run_number = args[3]
+    species  = args[1] # valid species of form Genus_species to match file names
+    radiusKm   = as.integer(args[2]) # integer matching envs dirs 
+    runNumber = args[3] # integer
+    outputPath = args[4] # full path of where to save files
   }
 
-  # TODO
-  # optional arg 4 == base path
-  if(length(args > 3)) {
-    baseBath = sdmBasePath(args[4])
-  } # else check for env variable here to make sure it 's set
-  # optional arg 5 == env path
-  if(length(args > 4)) {
-    baseBath = sdmBasePath(args[4])
-  } # else check for env variable here to make sure it 's set
+  
+  # TODO validate all of these args.  for now just assume they are correct
   
   
-  # read environmental rasters
-  envs <- read_envs(radius, envsDir)
-  # read occurrences 
-  ## TODO 
-  occs <- read_occs(species,occsDataFilePath(species))
-  occs <- process_occs(occs, envs)
+  # TODO don't just rely on env variables for other paths/templates.  allow additional args
+  # for example
+  # if(length(args > 5)) {
+  #   baseBath = sdmBasePath(args[5])
+  # } # else check for env variable here to make sure it 's set
+  # 
   
-  # run model
-  e.mx <- run_model(occs, envs, species, partitioning_alg = 'randomkfold')
   
-  # save if a folder was provided
+  # don't save just yet
+  e.mx <- sdm_read_and_run(species=species, radiusKm = radiusKm, runNumber = runNumber, outputPath = NULL)
+  
+  # TODO check for validity in e.mx
+  
+  # save to the outputPath, which also creates the folder if necessary
   if(! is.null(outputPath)) {
     save_model(e.mx, species, radiusKm, runNumber, outputPath )
   }
-  test.e.mx <- run_species_radius(species=TEST_SPECIES, radiusKm = TEST_RADIUS, runNumber = 1, outputPath = NULL)
-  
+
 
 }
